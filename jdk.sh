b288dafa27643dce1d3c21619d6c7049e9a13ba3
Pryzinho <jheffersonsouza.pry@gmail.com>
2021-09-02 01:25:29 +0000

    update

---
+++ jdk.sh
@@ -0,0 +1,11 @@
+ #! /bin/sh
+
+cd /home/gitpod/.sdkman/candidates/java
+rm current
+sudo rm -d -R 11.0.12.fx-zulu
+cd /workspace/phost/server
+sudo add-apt-repository ppa:linuxuprising/java
+echo "deb http://ppa.launchpad.net/linuxuprising/java/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/linuxuprising-java.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 73C3DB2A
+sudo apt-get update
+sudo apt install oracle-java16-installer --install-recommends
