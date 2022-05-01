ba8ebb96e62acf918b8b596771d062dc1bafde06
Pryzinho <jheffersonsouza.pry@gmail.com>
2022-05-01 17:11:24 +0000

    Update to 1.18.2

--- server/start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Server 1.18.1 - 1Gb
-:start
-java -Xms1G -Xmx2G -jar paper.jar -nogui
-pause
-goto start
---
+++ server/start.sh
@@ -0,0 +1,2 @@
+ #! /bin/sh
+ java -Xms1G -Xmx4G -Dfile.enconding=UTF-8 -jar paper.jar --nogui
